059046a792e9cd554e27bc6d2c087d82a9eae21a
Nyla <bhargavanyla@gmail.com>
2025-12-25 22:45:42 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,13 @@
-import random
-import numpy as np
-import torch
+import random, torch, numpy as np
 from sklearn.metrics import roc_auc_score, average_precision_score
 
-
-def set_seed(seed=42):
+def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
 
 def compute_metrics(y_true, y_prob):
     return {
-        "auc": roc_auc_score(y_true, y_prob),
-        "aupr": average_precision_score(y_true, y_prob)
+        "roc_auc": roc_auc_score(y_true, y_prob),
+        "pr_auc": average_precision_score(y_true, y_prob)
     }
