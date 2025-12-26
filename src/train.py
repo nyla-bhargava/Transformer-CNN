b76dc9253b8ddd150041493d9b05c3fc71c9a345
Nyla <bhargavanyla@gmail.com>
2025-12-26 14:57:56 +0530

    Remove duplicate set_seed call in train.py

--- src/train.py
+++ src/train.py
@@ -3,8 +3,11 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from dataset import OffTargetDataset
 from model import CNNOnly, TransformerOnly, TransformerCNN
-from utils import set_seed, compute_metrics
+from utils import compute_metrics
 import pandas as pd
+from utils import set_seed
+
+set_seed(42)
 
 def train_model(config):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
